610af91d174fbb230d5ca5d6302ffba0b2c714a6
Dje Par <jparadis21@hotmail.com>
2022-04-24 17:59:15 -0400

    Iterator stuff

---
+++ IntroProg/iterator-rust/target/debug/deps/iterator_rust-130efbf8b285baf0.d
@@ -0,0 +1,5 @@
+C:\Users\jpara\Documents\Etudes\CoursGenInfo\IntroProg\iterator-rust\target\debug\deps\iterator_rust-130efbf8b285baf0.exe: src\main.rs
+
+C:\Users\jpara\Documents\Etudes\CoursGenInfo\IntroProg\iterator-rust\target\debug\deps\iterator_rust-130efbf8b285baf0.d: src\main.rs
+
+src\main.rs:
---
+++ IntroProg/iterator-rust/target/debug/iterator-rust.d
@@ -0,0 +1,1 @@
+C:\Users\jpara\Documents\Etudes\CoursGenInfo\IntroProg\iterator-rust\target\debug\iterator-rust.exe: C:\Users\jpara\Documents\Etudes\CoursGenInfo\IntroProg\iterator-rust\src\main.rs
