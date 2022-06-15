69b0212e5f147f8145a9a3c67f091a5e7a2ced27
Dje Par <jparadis21@hotmail.com>
2022-06-15 03:21:56 -0400

    Python codewars

--- IntroProg/python/codewars/interlockable.py
+++ IntroProg/python/codewars/interlockable.py
@@ -3,4 +3,19 @@ Interlokcing Binary Pairs
 """
 
 def interlockable(a,b):
-    
+    a = str(bin(a))
+    b = str(bin(b))
+    Value = True
+    index = 0
+    if a < b:
+        smallest = a
+    else:
+        smallest = b
+    for i in range(1, len(smallest), 1):
+        print("a = {} and b = {}".format(a[-i], b[-i]))
+        if a[-i] == 1 and b[-i] == 1:
+            Value = False
+            print(Value)
+    return Value
+
+print(interlockable(3, 6))
---
+++ IntroProg/python/codewars/ipv4_address_class.py
@@ -0,0 +1,16 @@
+'''
+networking 101/2: Identify the IPv4 address Class
+
+
+def ipv4_address_class(ipv4_addr):
+    bonjour = ipv4_addr.split(".")
+    print(bonjour[0])
+    return bonjour
+
+ipv4_address_class("192.0.0.0")
+'''
+
+var = 5
+
+match var:
+    case 
