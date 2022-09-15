5392950d6f46e16558995ef4884fa34400fccd12
djepar <90092257+djepar@users.noreply.github.com>
2022-09-16 00:05:28 +0800

    jest

--- web-dev/MetaJs/Week3/addFive.test.js
+++ web-dev/MetaJs/Week3/addFive.test.js
@@ -1,7 +1,7 @@
 const { default: TestRunner } = require('jest-runner');
 const addFive = require(`./addFive.js`);
 
-test('returns the number plus 5', () =>{
+test('returns the number plus 5', () => {
     expect(addFive(1)).toBe(6);
   
 })
