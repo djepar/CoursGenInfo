364eee9b15c68d34a8f225aba191cabc9dafca02
Dje Par <jparadis21@hotmail.com>
2022-10-08 10:05:04 +0800

    Challenge isPalindrome done

--- Algorithms/LeetCode/PalindromeNumber/FirstTry.js
+++ Algorithms/LeetCode/PalindromeNumber/FirstTry.js
@@ -2,7 +2,20 @@
  * @param {number} x
  * @return {boolean}
  */
- var isPalindrome = function() {
+ var isPalindrome = function(x) {
+    let xString = x.toString();
+    xString = xString.split("");
+    //console.log(xString)
+    for (let i = 0; i < xString.length; i++){
+        let negIndex = (xString.length - (i+1))
+        if (xString[i] !== xString[negIndex]){
+            return false
+            //console.log(xString[i], xString[negIndex])
+        }
+    }
     return true
 };
 
+
+console.log(isPalindrome(12345))
+console.log(isPalindrome(2222))
