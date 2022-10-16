5d50bed29097338e523bffadde47cbb2fe0930d2
Dje Par <jparadis21@hotmail.com>
2022-10-16 17:55:31 +0800

    Trying to solve roman numeral encoder in vain

--- Programmation/JavaScript/CodeWars/romanNumeralEncoder.js
+++ Programmation/JavaScript/CodeWars/romanNumeralEncoder.js
@@ -11,13 +11,13 @@ let dicSym = {
   "X": 10,
   "V": 5,
   "I": 1,
-}
-
-
+};
+let listSym = ["M", "D", "C", "L", "X", "V","I"];
 
 //Pass the array to see if there is less than 3 consecutive symbols
 function MoreThree(arrayNum, target){
   if (arrayNum[arrayNum.length -1] === arrayNum[arrayNum.length  -2] &  arrayNum[arrayNum.length  -2] === arrayNum[arrayNum.length  -3]){
+    
     if(arrayNum[arrayNum.length -1] === target){
       return true
     }
@@ -31,26 +31,42 @@ const test2 = ["C"]
 // console.log(MoreThree(test1));
 // console.log(MoreThree(test2));
 function nextSymbol(num, symArrays){
-  for (el in dicSym){
-    console.log(el)
-    if (dicSym[el] <= num){
-      if (!(MoreThree(symArrays, el))){
-        return el
+  for (let i = 0; i < listSym.length; i++){
+    if (dicSym[listSym[i]] <= num){
+      //if (!(MoreThree(symArrays, listSym[i]))){
+      if (symArrays[symArrays.length -1] === symArrays[symArrays.length  -2] &  symArrays[symArrays.length  -2] === symArrays[symArrays.length  -3]){
+        if ((dicSym[listSym[i]] + dicSym[listSym[i-1]])  <= num){
+          let newlist = symArrays;
+          newlist.pop;
+          newlist.pop;
+          newlist.pop;
+          newlist.push(listSym[i-1]);
+          newlist.push(listSym[i])
+          return newlist
+        }
+        return listSym[i]
       }
     }
   }
 }
 
-// console.log(nextSymbol(symbols, 1050, ["M", "M", "M"]))
-console.log(nextSymbol(1663, []))
+console.log(nextSymbol(1050, ["M", "M", "M"]))
+console.log(nextSymbol(4, ["I", "I", "I"]))
+
+//console.log(nextSymbol(1663, []))
 function solution(number){
   let currentValue = number;
   let symArrays = [];
-  // while (currentValue !== 0){
-  //   let tempSym = nextSymbol(currentValue, symArrays);
-  //   console.log(tempSym)
-  //   currentValue -= symbols[]
-  // }
+  while (currentValue !== 0){
+    let tempSym = nextSymbol(currentValue, symArrays);
+    console.log(tempSym)
+    currentValue -= dicSym[tempSym]
+    symArrays.push(tempSym)
+    console.log(currentValue)
+
+  }
+  return symArrays
 }
+//console.log(solution(99))
 
 
